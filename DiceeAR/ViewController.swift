15c48e230c00f4034e5b8a44b411a4624c81f6be
Dawid Kubicki <dawid@pluscode.io>
2021-01-03 20:38:21 +0100

    plane detection

--- DiceeAR/ViewController.swift
+++ DiceeAR/ViewController.swift
@@ -16,17 +16,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.automaticallyUpdatesLighting = true
         
-        // Set the scene to the view
-        sceneView.scene = scene
+    
+//        let diceScene = SCNScene(named: "art.scnassets/dice.scn")!
+//
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//
+//        diceNode.position = SCNVector3(x: 0, y: 0, z:-0.1)
+//
+//        sceneView.scene.rootNode.addChildNode(diceNode)
+//        }
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +42,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        
+        //plane detection
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -44,6 +57,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if anchor is ARPlaneAnchor {
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y:0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIColor(named: "red")
+            plane.materials = [gridMaterial]
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
+        }
     }
 
     // MARK: - ARSCNViewDelegate
